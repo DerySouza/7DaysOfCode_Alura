aade0240375cbc334493ecc4e6aa32d2726526cc
Dery <derykatze@hotmail.com>
2022-04-08 20:27:57 -0300

    Commit-inicial

---
+++ src/main/Busca.java
@@ -0,0 +1,43 @@
+package Desafio.API.Desafio_API;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Busca {
+	
+ private static final String API_URL = "https://imdb-api.com/en/API/Top250Movies/API_KEY";
+    
+	public CompletableFuture<List<String>> fetchReporterArea() {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(API_URL))
+                .build();
+        return client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenApply(Busca::parse);
+    }
+	public static List<String> parse(String dataset) {
+
+        JSONObject filmes = new JSONObject(dataset);
+
+        List<String> list = new ArrayList<String>();
+        JSONArray array = filmes.getJSONArray("items");
+
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject fila = array.getJSONObject(i);
+            
+            list.add(fila.optString("rank") + "; " + fila.optString("title"));
+        }
+        
+        return list;
+    }
+}
---
+++ src/main/Teste.java
@@ -0,0 +1,36 @@
+package Desafio.API.Desafio_API;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+
+
+ 
+public class Teste {
+	
+	
+	
+    public static void main( String[] args ) throws IOException, InterruptedException, ExecutionException {
+    	 
+    	Busca rankDeFilmes = new Busca();
+        CompletableFuture<List<String>> asyncResult = rankDeFilmes.fetchReporterArea();
+        
+        asyncResult.get().forEach(System.out::println);
+		 
+    }
+      
+}
